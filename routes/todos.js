bc2d5eb22444592281c168e0604e1aec42b78e83
Dan Moore <github@mooreds.com>
2021-02-22 14:53:09 -0700

    Added API.

--- routes/todos.js
+++ routes/todos.js
@@ -4,11 +4,24 @@ const express = require('express');
 // Router
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+getTodos = () => {
   todos = [];
   todos.push({'task': 'Get milk', 'done' : true});
   todos.push({'task': 'Read OAuth book', 'done' : false});
+  return todos;
+}
+
+router.get('/', (req, res, next) => {
+  todos = getTodos();
   res.render('index', {title: 'Todos', todos: todos});
 });
+
+router.get('/api', (req, res, next) => {
+  todos = getTodos();
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(todos));
+});
+
+
 
 module.exports = router;
