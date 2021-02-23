67224bd66025911c6f59f1e1e3bdf4a2a3d4ccfc
Dan Moore <github@mooreds.com>
2021-02-23 16:12:07 -0700

    Added auth server url.

--- routes/config.js
+++ routes/config.js
@@ -2,5 +2,6 @@ module.exports = {
   clientId : 'd67e4208-6722-4783-9787-03c3ead5230d',
   issuer : 'https://local.fusionauth.io',
   clientSecret : 'CmW5LwOKNrqFNHfEF8O8gLhMXAHP4sz2Gm4PEm6baNA',
+  authServerUrl : 'https://local.fusionauth.io',
 }
 
